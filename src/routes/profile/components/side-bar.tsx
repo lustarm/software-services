6f552a6b9d36cc894cbd9f674a786defa80a59cf
jeweler <valve.valve101@gmail.com>
2025-01-10 19:17:28 -0500

    sidebar stuff

--- src/routes/profile/components/side-bar.tsx
+++ src/routes/profile/components/side-bar.tsx
@@ -1,63 +1,32 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function ProfileSideBar() {
+    const location = useLocation()
+
+    const isActive = (path: string) =>
+        location.pathname.replace(/\/+$/, '') === path.replace(/\/+$/, '')
+
     return (
         <div className="bg-zinc-900 text-zinc-200 p-5 h-full w-56 my-6 rounded-md">
             <ul className="space-y-4">
                 <li className="rounded-md overflow-hidden">
                     <Link
                         to="/profile/"
-                        className="block p-3 bg-zinc-800 hover:bg-zinc-700 rounded-md transition"
+                        className={`block p-3 hover:bg-zinc-700 rounded-md
+                        transition
+                        ${isActive("/profile/") ? 'bg-zinc-500' : 'bg-zinc-800'}`}
                     >
                         Profile
                     </Link>
                 </li>
                 <li className="rounded-md overflow-hidden">
                     <Link
-                        to="schedule/"
-                        className="block p-3 bg-zinc-800 hover:bg-zinc-700 rounded-md transition"
+                        to="/profile/schedule/"
+                        className={`block p-3 hover:bg-zinc-700 rounded-md
+                        transition
+                        ${isActive("/profile/schedule/") ? 'bg-zinc-500' : 'bg-zinc-800'}`}
                     >
                         Schedule
-                    </Link>
-                </li>
-                <li className="rounded-md overflow-hidden">
-                    <Link
-                        to=""
-                        className="block p-3 bg-zinc-800 hover:bg-zinc-700 rounded-md transition"
-                    >
-                        Salary Details
-                    </Link>
-                </li>
-                <li className="rounded-md overflow-hidden">
-                    <Link
-                        to=""
-                        className="block p-3 bg-zinc-800 hover:bg-zinc-700 rounded-md transition"
-                    >
-                        Time Off
-                    </Link>
-                </li>
-                <li className="rounded-md overflow-hidden">
-                    <Link
-                        to=""
-                        className="block p-3 bg-zinc-800 hover:bg-zinc-700 rounded-md transition"
-                    >
-                        Documents
-                    </Link>
-                </li>
-                <li className="rounded-md overflow-hidden">
-                    <Link
-                        to=""
-                        className="block p-3 bg-zinc-800 hover:bg-zinc-700 rounded-md transition"
-                    >
-                        Training
-                    </Link>
-                </li>
-                <li className="rounded-md overflow-hidden">
-                    <Link
-                        to="benefits/"
-                        className="block p-3 bg-zinc-800 hover:bg-zinc-700 rounded-md transition"
-                    >
-                        Benefits
                     </Link>
                 </li>
             </ul>
